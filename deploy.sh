7616eff15c0eb85541201fd1afc6e6802d6a3c49
jas <reducm@gmail.com>
2012-07-25 14:44:39 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,14 @@ if [ -d "$HOME/.vim/bundle/" ];then
 else
     echo "cant find the vundle! fetch it on github now!"
     git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
-    if [ -L "$HOME/.vimrc" ];then
-	rm $HOME/.vimrc
-	ln -s ./vimrc $HOME/.vimrc
-    else
-	ln -s ./vimrc $HOME/.vimrc
-    fi
+    rm $HOME/.vimrc
+    ln -s $PWD/vimrc $HOME/.vimrc
     echo "already create symoble link to vimrc, you need to opren vim and run :BundleInstall first, then go back to run this shell again!"
     exit 1
 fi
 
 echo "create the vimrc softlink now..."
-rm $HOME/.vimrc
+rm -f $HOME/.vimrc
 ln -s $PWD/vimrc $HOME/.vimrc
 echo "create success! now copy the snippets to your $HOME/.vim"
 
